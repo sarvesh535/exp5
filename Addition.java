3905f7d0d1af237a12e60a2be5c682f0fb4685d0
sarvesh535 <perugupallisarvesh@gmail.com>
2026-08-20 15:30:35 +0530

    Update Addition.java

--- Addition.java
+++ Addition.java
@@ -1,13 +1,10 @@
-public class Addition{
+public class Addition {
     public static void main(String[] args) {
-        // Declaring and initializing predefined numbers
         int firstNumber = 15;
         int secondNumber = 25;
-        
-        // Performing addition
+
         int sum = firstNumber + secondNumber;
-        
-        // Displaying the output
+
         System.out.println("The sum of " + firstNumber + " and " + secondNumber + " is: " + sum);
     }
 }
